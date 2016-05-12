c4c2b1253c9889ca71b209ee79157224066c747f
tnarla <trushithanarla@gmail.com>
2016-05-12 00:05:25 -0400

    added bootstrap

---
+++ popcrn/static/js/map.js
@@ -0,0 +1,18 @@
+var format = function(d) {
+    d = d / 1000000;
+    return d3.format(',.02f')(d) + 'M';
+}
+
+var map = d3.geomap.choropleth()
+    .geofile('../static/topojson/world/countries.json')
+    .colors(colorbrewer.RdPu[9])
+    .column('YR2010')
+    .format(format)
+    .legend(true)
+    .unitId('Country Code');
+
+d3.csv('../static/data/sample.csv', function(error, data) {
+    d3.select('#map')
+        .datum(data)
+        .call(map.draw, map);
+});
