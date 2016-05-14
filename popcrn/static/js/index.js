95989f24e2da8fa85d9377ff7ed613e86b014e20
tnarla <trushithanarla@gmail.com>
2016-05-13 22:30:57 -0400

    h

--- popcrn/static/js/index.js
+++ popcrn/static/js/index.js
@@ -1,13 +1,14 @@
 $(document).ready(function() {
   var btn = document.getElementById("btnSearch");
+  var search = document.getElementById("txtSearch");
   btn.addEventListener("click", function() {
-    var val = btn.value;
+    var val = search.value;
     console.log(val);
     if (val == null || val.trim() == "") {
       alert("Enter a valid query into the search box!");
     } else {
       console.log("Post request!");
-      $.post("http://localhost:6543/?topic=" + val, {"text":text}, function(data) {
+      $.post("http://localhost:6543/?topic=" + val, function(data) {
 				console.log(data);
                 mapcreate(data);
 			});
